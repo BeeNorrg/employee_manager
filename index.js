7abb413d5e263352a7662847ca332af87a89985c
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-06-03 22:19:26 -0500

    added inquirer prompt for employee manager

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-const figlet = require('figlet');
 const inquirer = require('inquirer');
-const prompt = require('./assets/prompt');
 const add = require('./assets/add');
 const remove = require('./assets/remove');
+const review = require('./assets/review');
+const connection = require('./server');
 
-const init = () => {
-    inquirer.prompt([
-        {
-            
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Loading Employee Management System...');
+    //artificial loading time
+    function dramaticPause(){
+        setTimeout( () => {runInquiry()}, 300);
+    }
+    prompt();
+})
+
+const prompt = () => {
+    inquirer.prompt({
+        name: 'Employee Manager v1.0.0',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'View Employees',
+            'View Roles',
+            'View Departments',
+            'Edit Employees',
+            'Exit'
+        ]
+    }).then((answer) => {
+        switch(answer.choice){
+            case 'Add Employee':
+
+                break;
+            case 'Add Role':
+                
+                break;
+            case 'Add Department':
+                break;
+            case 'View Employees':
+                break;
+            case 'View Rolse':
+                break;
+            case 'View Departments':
+                break;
+            case 'Edit Employees':
+                break;
+            case 'Exit':
+                connection.end();
+                break;
         }
-    ])
+    })
 }
+
+
+
